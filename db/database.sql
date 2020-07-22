0fd775253fffc224eab5c59f26027a1e7a0554c3
Rafael Carmona Castruita <rccastruita@gmail.com>
2020-07-22 16:14:31 -0500

    Added id column to user

--- db/database.sql
+++ db/database.sql
@@ -7,12 +7,15 @@ GRANT ALL PRIVILEGES ON onlineStore.* TO 'adminTienda'@'localhost';
 use onlineStore;
 
 CREATE TABLE user(
+    id INT NOT NULL AUTO_INCREMENT,
     email varchar(150) NOT NULL,
     password varchar(60) NOT NULL,
     name varchar(150) NOT NULL,
     role ENUM('client', 'admin') DEFAULT 'client',
-    PRIMARY KEY (email)
+    PRIMARY KEY (id)
 );
+
+ALTER TABLE user AUTO_INCREMENT = 1000;
 
 CREATE TABLE product(
 	id INT NOT NULL AUTO_INCREMENT,
@@ -54,20 +57,20 @@ CREATE TABLE product_on_platform(
 
 CREATE TABLE cart_item(
 	id INT NOT NULL AUTO_INCREMENT,
-    user_email varchar(150) NOT NULL,
+    user_id varchar(150) NOT NULL,
     product_id INT NOT NULL,
     quantity INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_email) REFERENCES user(email),
+    FOREIGN KEY (user_id) REFERENCES user(id),
     FOREIGN KEY (product_id) REFERENCES product(id)
 );
 
 CREATE TABLE purchase(
     id INT NOT NULL AUTO_INCREMENT,
-    user_email varchar(150) NOT NULL,
+    user_id varchar(150) NOT NULL,
     purchase_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_email) REFERENCES user(email)
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
 CREATE TABLE purchase_item(
@@ -92,10 +95,10 @@ CREATE TRIGGER before_user_delete
 BEFORE DELETE ON user FOR EACH ROW
 BEGIN
 	DELETE FROM cart_item
-	WHERE user_email = OLD.email;
+	WHERE user_id = OLD.id;
 
 	DELETE FROM purchase
-	WHERE user_email = OLD.email;
+	WHERE user_id = OLD.id;
 END$$
 
 DELIMITER ;
